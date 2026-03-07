7b7835a0b9574bf4b8763a023cff3c2e1ccf4b55
floydsimmy <104207234+floydsimmy@users.noreply.github.com>
2026-03-07 23:39:11 +0000

    built the skeleton and main basic loop

--- cluedo/src/game/engine.py
+++ cluedo/src/game/engine.py
@@ -9,3 +9,39 @@
 #
 # The engine uses the models from models.py to represent game state,
 # and checks rules.py to decide what is and isn't allowed.
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500,500))
+Caption = pygame.display.set_caption("Cluedo")
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+run = True
+while run:
+    pygame.time.delay(100)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+
+    win.fill((0,0,0))
+    pygame.draw.rect(win, (255,0,0), (x,y,width,height))
+    pygame.display.update()
+
+    pygame.quit()
